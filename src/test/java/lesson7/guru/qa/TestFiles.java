0f4298c2685efd06e2dc20373de8108d8e4c0856
Артемий <rick-glat@mail.ru>
2021-11-22 15:42:17 +0300

    add Xlsx Test

--- src/test/java/lesson7/guru/qa/TestFiles.java
+++ src/test/java/lesson7/guru/qa/TestFiles.java
@@ -1,17 +1,23 @@
 package lesson7.guru.qa;
 
+import com.codeborne.pdftest.PDF;
+import com.codeborne.selenide.ClickOptions;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.xlstest.XLS;
 import com.codeborne.selenide.FileDownloadMode;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.apache.commons.io.IOUtils;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.jupiter.api.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.files.DownloadActions.click;
 
 public class TestFiles {
     @BeforeAll
@@ -20,15 +26,40 @@ public class TestFiles {
     }
 
     @Test
-    @DisplayName("Скачивание файла doc")
-    void DownloadFIleFormatDocTest() throws FileNotFoundException {
-        Configuration.proxyEnabled = true;
-        Configuration.fileDownload = FileDownloadMode.PROXY;
+    @DisplayName("Скачивание файла xls")
+    void DownloadFIleXlsTest() throws IOException {
+        open("https://kub-24.ru/prajs-list-shablon-prajs-lista-2020-v-excel-word-pdf/");
 
-        open("https://e-kontur.ru/Enquiry/DocumentTemplates");
-
-        $(byText("Договор об оказании услуг")).click();
-        File download = $(byText("Скачать шаблон")).download();
-        System.out.println(download.getAbsolutePath());
+        File xlsxFile = $(byText("шаблон прайс-листа в Excel")).download();
+        XLS parsedXlsx = new XLS(xlsxFile);
+        boolean checkFile = ((XSSFWorkbook) parsedXlsx.excel)
+                .getSheetAt(0)
+                .getRow(12)
+                .getCell(1)
+                .getStringCellValue()
+                .contains("Кирпич строительный двойной щелевой М150 ( 250х120х130мм)");
     }
+
+    @Test
+    @DisplayName("Скачивание файла readme")
+    @Disabled
+    void DownloadFileReadmeTest() throws IOException {
+        open("https://github.com/dtuchs/epam-hackathon/blob/master/README.md");
+
+        File download = $("#raw-url").download();
+        String fileContent = IOUtils.toString(new FileReader(download));
+        Assertions.assertTrue(fileContent.contains("Сборка в JENKINS http://178.250.156.47:8081/job/Project_23"));
+    }
+
+    @Test
+    @DisplayName("Скачивание файла pdf")
+    void DownloadFIlePdfTest() throws IOException {
+        open("https://kub-24.ru/prajs-list-shablon-prajs-lista-2020-v-excel-word-pdf/");
+
+        File pdfFile = $(byText("шаблон прайс-листа в PDF")).download();
+        PDF parsedPdf = new PDF(pdfFile);
+        Assertions.assertEquals(1, parsedPdf.numberOfPages);
+    }
+
+
 }
